d6542dbc6878c69e471010af91ceb2960eb36904
Your Name <you@example.com>
2023-07-24 23:44:38 -0500

    Added Code

---
+++ src/test/java/ITDeepThought.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class ITDeepThought {
+	@Test
+	public void IntegrationDeepThought() {
+		System.out.println("This is Integration Test 1\n");
+	}
+	@Test
+	public void IntegrationDeepThought2() {
+		System.out.println("This is Integration Test 2\n");
+	}	
+}
